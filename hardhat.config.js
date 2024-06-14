ed9e4df52ee65630af9a5a8eea425c14b8634472
Sarmad Ahmad <sarmadsmac@Sarmads-MacBook-Air.local>
2024-06-14 14:22:59 +0200

    init

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("./tasks/block-number");
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.EHTERSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+    },
+    solidity: "0.8.8",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const { ethers, run, network } = require("hardhat");
+async function main() {
+    const simpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
+    console.log("Deploying Contract..");
+    const simpleStorage = await simpleStorageFactory.deploy();
+    await simpleStorage.getDeployedCode();
+    const address = await simpleStorage.getAddress();
+    console.log(`Deployed Conctract to ${address}`);
+    if (network.config.chainId === 11155111 && process.env.EHTERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...");
+        await simpleStorage.deploymentTransaction().wait(6);
+        await verify(address, []);
+    }
+    const currentValue = await simpleStorage.retrieve();
+    console.log("currentValue " + currentValue);
+
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log("updated value " + updatedValue);
+}
+
+async function verify(contractAddress, args) {
+    console.log("verifiny contract....");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified");
+        } else {
+            console.log(error);
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
